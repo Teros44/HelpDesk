be3ac968b1ec157a945bcd0eb38d66fd20512831
Teros44 <58023883+Teros44@users.noreply.github.com>
2019-12-24 14:51:27 +1000

    upd

---
+++ models/User.js
@@ -0,0 +1,15 @@
+﻿const users = [];
+
+module.exports = class User{
+
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    save(){
+        users.push(this);
+    }
+    static getAll(){
+        return users;
+    }
+}
