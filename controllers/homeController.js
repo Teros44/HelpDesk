e602cc647d26dae44c8ea52c66b8751357bd53d8
Teros44 <58023883+Teros44@users.noreply.github.com>
2019-12-29 22:38:23 +1000

    cookie signed

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -7,8 +7,14 @@ exports.about = function (request, response) {
     response.send("О сайте");
 };
 exports.login = function(request,response){
-  var cookies = new Cookies(request,response);
-    cookies.set('admin','true');
+  var cookies = new Cookies(request,response,{"keys":['44secrets']});
+  /*var cookieOptions = {
+    maxAge: 3600,
+    secure: false,
+    signed: true
+  };
+  */
+  cookies.set('admin',[true],[maxAge=60]);
     console.log(cookies.get('admin'));
     response.send(request.body);
 };
