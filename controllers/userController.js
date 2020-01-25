899da26ad121c4004470aa662ca2d04b23e5278b
Teros44 <58023883+Teros44@users.noreply.github.com>
2020-01-25 14:45:33 +1000

    2.3 up

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,44 +1,46 @@
 ﻿const User = require("../models/user.js");
- 
-exports.addUser = function (request, response){
-    response.render("create.hbs");
+
+exports.addUser = function(request, response) {
+  response.render("create.hbs");
 };
-exports.getUsers = function(request, response){
-    console.log(request.session);
-    console.log(request.session.admin);
-    if(request.session.admin=="Да"){
-        User.find({}, function(err, allUsers){
-  
-            if(err) {
-                console.log(err);
-                return response.sendStatus(400);
-            }
-            response.render("users.hbs", {
-                users: allUsers
-            });
-        });
-    } else {
-        response.redirect("/logoff");
-    };
-    
-};
-
-
-exports.postUser= function(request, response){
-    if(request.session.admin=="Да"){
-        if(!request.body) return response.sendStatus(400);
-        const userFIO = request.body.FIO;
-        const userLogin = request.body.login;
-        const userPassword = request.body.password;
-        const userAdmin = request.body.admin;
-
-        const user = new User({FIO: userFIO, login: userLogin, password: userPassword, admin: userAdmin});
-     
-        user.save(function(err){
-        if(err) return console.log(err);
-        response.redirect("/users");
+exports.getUsers = function(request, response) {
+  console.log(request.session);
+  console.log(request.session.admin);
+  if (request.session.admin == "Да") {
+    User.find({}, function(err, allUsers) {
+      if (err) {
+        console.log(err);
+        return response.sendStatus(400);
+      }
+      response.render("users.hbs", {
+        users: allUsers
+      });
     });
-    } else {
-        response.redirect("/logoff");
-    };
+  } else {
+    response.redirect("/logoff");
+  }
+};
+
+exports.postUser = function(request, response) {
+  if (request.session.admin == "Да") {
+    if (!request.body) return response.sendStatus(400);
+    const userFIO = request.body.FIO;
+    const userLogin = request.body.login;
+    const userPassword = request.body.password;
+    const userAdmin = request.body.admin;
+
+    const user = new User({
+      FIO: userFIO,
+      login: userLogin,
+      password: userPassword,
+      admin: userAdmin
+    });
+
+    user.save(function(err) {
+      if (err) return console.log(err);
+      response.redirect("/users");
+    });
+  } else {
+    response.redirect("/logoff");
+  }
 };
